93e0151861c0044a3ddce2cdddafed2d2cd2909e
jameskyokushin <james@kyokushin.com.ph>
2012-02-10 13:44:42 +0800

    form ready/can save/see the list of save/can show single transaction

--- app/controllers/marketings_controller.rb
+++ app/controllers/marketings_controller.rb
@@ -1,10 +1,27 @@
 class MarketingsController < ApplicationController
 
-	def index
 
+
+	def save
+
+  end
+
+	def index
+		@marketings = Marketing.all
 	end
 	
 	def new
- 		@marketings = Marketing.new
+ 		@marketing = Marketing.new
   end
+
+	def create
+		@marketing = Marketing.new(params[:marketing])
+		@marketing.save
+		flash[:notice] = "Save to the Database"
+		render :action => "save"
+	end
+	
+	def show
+		@marketing = Marketing.find(params[:id])
+	end
 end
