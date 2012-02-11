e8a68bacb9a17e917660d3edcecc90baffa152ff
jameskyokushin <james@kyokushin.com.ph>
2012-02-11 10:11:11 +0800

    creation of sales call function properly

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,7 +2,16 @@ class SalesController < ApplicationController
 	def new
 		@sale = Sale.new
 	end
-	
+	def create
+		@sale = Sale.new(params[:sale])
+		if @sale.save
+			flash[:notice] = "Sales Call Successfully Saved."
+			render :action => "show"
+		else
+			flash[:alert] = "Sales Call Not Saved"
+			render :action => "new"
+		end
+	end
 	
 	
 end
